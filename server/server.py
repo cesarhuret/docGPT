6dcdeaa63d31afa50c6d3108e24fac8adb91f41a
cesar <cesar.huret@gmail.com>
2022-12-06 01:26:06 +0000

    Switch to PyChatGPT

--- server/server.py
+++ server/server.py
@@ -3,30 +3,29 @@ from dotenv import load_dotenv
 import os
 import flask
 from flask import Flask
-from revChatGPT.revChatGPT import Chatbot
+from PyChatGPT import pychat as PyChatGPT
+
+# Fancy stuff
+import colorama
+from colorama import Fore
+
+colorama.init(autoreset=True)
 
 load_dotenv()
 
 app = Flask(__name__)
-token = os.environ['session_token']
 
-config = {
-  "Authorization": "<API_KEY>",  # This is optional
-  "session_token": token  # This is used to refresh the authentication
-}
-
+gptBot = PyChatGPT.PyChatGPT()
 
 @app.route("/chat", methods=["POST"])
 def index():
   message = flask.request.json.get("message")
-  chatbot = Chatbot(config, conversation_id=None)
-  chatbot.reset_chat()  # Forgets conversation
-  chatbot.refresh_session()  # Uses the session_token to get a new bearer token
-  resp = chatbot.get_chat_response(message)
-  return resp['message']
+  response = gptBot.conversate(message)
+  return response
 
-
-if __name__ == "__main__":
-  from waitress import serve
-  print("Server started at http://localhost:8080")
-  serve(app, host="0.0.0.0", port=8080)
+def application():
+  success = gptBot.initialise()
+  if(success):
+    return app
+  else:
+    exit(1)
