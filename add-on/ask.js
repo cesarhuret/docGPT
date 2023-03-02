dd27916913ec85b157766145f9c662fc649baf76
Cesar Huret <67405604+cesarhuret@users.noreply.github.com>
2023-03-02 17:51:07 +0000

    Update ask.js

--- add-on/ask.js
+++ add-on/ask.js
@@ -1,4 +1,20 @@
 /**
+ * Copyright Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+// [START apps_script_docs_translate_quickstart]
+/**
  * @OnlyCurrentDoc
  *
  * The above comment directs Apps Script to limit the scope of file
@@ -19,33 +35,53 @@
  */
 function onOpen(e) {
   DocumentApp.getUi().createAddonMenu()
-      .addItem('Ask', 'ask')
+      .addItem('Start', 'showSidebar')
+      .addItem('Ask', 'autoComplete')
       .addToUi();
+  showSidebar();
 }
 
-function ask() {
-
-  const selectedText = getSelectedText();
-  const request = sendRequest(selectedText[0]);
-  insertText('\n' + request + '\n')
-
+/**
+ * Runs when the add-on is installed.
+ * This method is only used by the regular add-on, and is never called by
+ * the mobile add-on version.
+ *
+ * @param {object} e The event parameter for a simple onInstall trigger. To
+ *     determine which authorization mode (ScriptApp.AuthMode) the trigger is
+ *     running in, inspect e.authMode. (In practice, onInstall triggers always
+ *     run in AuthMode.FULL, but onOpen triggers may be AuthMode.LIMITED or
+ *     AuthMode.NONE.)
+ */
+function onInstall(e) {
+  onOpen(e);
 }
 
-function sendRequest(selectedText) {
-  
-  var options = {
-    'method' : 'post',
-    'contentType': 'application/json',
-    // Convert the JavaScript object to a JSON string.
-    'payload' : JSON.stringify({
-      'message': selectedText
-    })
-  };
-  // Insert your server URL here
-  const response = UrlFetchApp.fetch(SERVER_URL, options);
-  return response
+function autoComplete() {
+
+  const selected = getSelectedText();
+
+  const response = getGPTResponse(selected[0]);
+
+  insertTextWithoutReplacement(response);
 }
 
+/**
+ * Opens a sidebar in the document containing the add-on's user interface.
+ * This method is only used by the regular add-on, and is never called by
+ * the mobile add-on version.
+ */
+function showSidebar() {
+  const ui = HtmlService.createHtmlOutputFromFile('sidebar')
+      .setTitle('docGPT');
+  DocumentApp.getUi().showSidebar(ui);
+}
+
+/**
+ * Gets the text the user has selected. If there is no selection,
+ * this function displays an error message.
+ *
+ * @return {Array.<string>} The selected text.
+ */
 function getSelectedText() {
   const selection = DocumentApp.getActiveDocument().getSelection();
   const text = [];
@@ -78,6 +114,53 @@ function getSelectedText() {
 }
 
 /**
+ * Gets the stored user preferences for the origin and destination languages,
+ * if they exist.
+ * This method is only used by the regular add-on, and is never called by
+ * the mobile add-on version.
+ *
+ * @return {Object} The user's origin and destination language preferences, if
+ *     they exist.
+ */
+function getPreferences() {
+  const userProperties = PropertiesService.getUserProperties();
+  return {
+    originLang: userProperties.getProperty('originLang'),
+    destLang: userProperties.getProperty('destLang')
+  };
+}
+
+/**
+ * Gets the user-selected text and translates it from the origin language to the
+ * destination language. The languages are notated by their two-letter short
+ * form. For example, English is 'en', and Spanish is 'es'. The origin language
+ * may be specified as an empty string to indicate that Google Translate should
+ * auto-detect the language.
+ *
+ * @param {string} origin The two-letter short form for the origin language.
+ * @param {string} dest The two-letter short form for the destination language.
+ * @param {boolean} savePrefs Whether to save the origin and destination
+ *     language preferences.
+ * @return {Object} Object containing the original text and the result of the
+ *     translation.
+ */
+function getGPTResponse(question) {
+
+  var options = {
+    'method' : 'post',
+    'contentType': 'application/json',
+    // Convert the JavaScript object to a JSON string.
+    'payload' : JSON.stringify({
+      'message': question
+    })
+  };
+  
+  const response = UrlFetchApp.fetch('https://chatgpt-api.kesarx.repl.co/chat', options);
+  const json = JSON.parse(response.getContentText());
+  return json.choices[0].message.content;
+}
+
+/**
  * Replaces the text of the current selection with the provided text, or
  * inserts text at the current cursor location. (There will always be either
  * a selection or a cursor.) If multiple elements are selected, only inserts the
@@ -87,6 +170,85 @@ function getSelectedText() {
  * @param {string} newText The text with which to replace the current selection.
  */
 function insertText(newText) {
+  const selection = DocumentApp.getActiveDocument().getSelection();
+  if (selection) {
+    let replaced = false;
+    const elements = selection.getSelectedElements();
+    if (elements.length === 1 && elements[0].getElement().getType() ===
+      DocumentApp.ElementType.INLINE_IMAGE) {
+      throw new Error('Can\'t insert text into an image.');
+    }
+    for (let i = 0; i < elements.length; ++i) {
+      if (elements[i].isPartial()) {
+        const element = elements[i].getElement().asText();
+        const startIndex = elements[i].getStartOffset();
+        const endIndex = elements[i].getEndOffsetInclusive();
+        element.deleteText(startIndex, endIndex);
+        if (!replaced) {
+          element.insertText(startIndex, newText);
+          replaced = true;
+        } else {
+          // This block handles a selection that ends with a partial element. We
+          // want to copy this partial text to the previous element so we don't
+          // have a line-break before the last partial.
+          const parent = element.getParent();
+          const remainingText = element.getText().substring(endIndex + 1);
+          parent.getPreviousSibling().asText().appendText(remainingText);
+          // We cannot remove the last paragraph of a doc. If this is the case,
+          // just remove the text within the last paragraph instead.
+          if (parent.getNextSibling()) {
+            parent.removeFromParent();
+          } else {
+            element.removeFromParent();
+          }
+        }
+      } else {
+        const element = elements[i].getElement();
+        if (!replaced && element.editAsText) {
+          // Only translate elements that can be edited as text, removing other
+          // elements.
+          element.clear();
+          element.asText().setText(newText);
+          replaced = true;
+        } else {
+          // We cannot remove the last paragraph of a doc. If this is the case,
+          // just clear the element.
+          if (element.getNextSibling()) {
+            element.removeFromParent();
+          } else {
+            element.clear();
+          }
+        }
+      }
+    }
+  } else {
+    const cursor = DocumentApp.getActiveDocument().getCursor();
+    const surroundingText = cursor.getSurroundingText().getText();
+    const surroundingTextOffset = cursor.getSurroundingTextOffset();
+
+    // If the cursor follows or preceds a non-space character, insert a space
+    // between the character and the translation. Otherwise, just insert the
+    // translation.
+    if (surroundingTextOffset > 0) {
+      if (surroundingText.charAt(surroundingTextOffset - 1) !== ' ') {
+        newText = ' ' + newText;
+      }
+    }
+    if (surroundingTextOffset < surroundingText.length) {
+      if (surroundingText.charAt(surroundingTextOffset) !== ' ') {
+        newText += ' ';
+      }
+    }
+    cursor.insertText(newText);
+  }
+}
+
+/**
+ * Inserts text after the current selected text.
+ *
+ * @param {string} newText The text with which to replace the current selection.
+ */
+function insertTextWithoutReplacement(newText) {
   const selection = DocumentApp.getActiveDocument().getSelection();
   if (selection) {
     const elements = selection.getSelectedElements();
